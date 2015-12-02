c42977aa7d45fecbad3b9fad869493a05f95cd05
ahmadaidin <ahmadaidin08.aa@gmail.com>
2015-12-02 23:53:41 +0700

    add Experiment dir and files

---
+++ Server.java
@@ -0,0 +1,12 @@
+public class Server{
+
+  public static void main(String[] args) {
+    ServerSocket MyService;
+    try {
+       MyServerice = new ServerSocket(PortNumber);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+  }
+}
